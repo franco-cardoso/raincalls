8ad5ef9f2e7e3733d7449dd8ddef71cb61272b51
franco-cardoso <francolkk788@gmail.com>
2023-03-23 20:17:40 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import Header from './components/header/Header.vue';
+</script>
+
+<template>
+  <header>
+    <Header></Header>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,46 @@
+<script></script>
+
+<template>
+    <div class="header-container">
+        <div>
+            <h1>LOGO</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><button>HOME</button></li>
+                <li><button>SERVICES & BENEFITS</button></li>
+                <li><button>WHY US</button></li>
+                <li><button>REVIEWS</button></li>
+                <li><button>CONTACT US</button></li>
+            </ul>
+        </nav>
+        <div></div>
+    </div>
+</template>
+
+<style scoped>
+.header-container {
+    display: grid;
+    height: 5rem;
+    grid-template-columns: 0.5fr 1fr 0.5fr;
+}
+.header-container div, .header-container nav {
+    display: flex;
+    align-items: center;
+}
+.header-container div:nth-child(1) {
+}
+.header-container div:nth-child(2) {
+    background-color: green;
+}
+
+button {
+    all: unset;
+}
+
+ul {
+    display: flex;
+    list-style: none;
+    gap: 20px;
+}
+</style>
