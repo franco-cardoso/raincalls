fda0dc46717002deff0616e5c0a3a371d9fe139d
franco-cardoso <francolkk788@gmail.com>
2023-03-24 12:55:00 -0300

    vue-gtag

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueGtag from "vue-gtag";
+
+createApp(App).use(VueGtag, {
+  config: { id: "G-QP22561NME" }
+}).mount("#app");
 
 createApp(App).mount('#app')
