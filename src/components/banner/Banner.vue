762798cc8cbc3c8b88e54c53a4b8c7e4ac0f7306
franco-cardoso <francolkk788@gmail.com>
2023-03-24 17:02:26 -0300

    video

--- src/components/banner/Banner.vue
+++ src/components/banner/Banner.vue
@@ -1,28 +1,32 @@
 <template>
     <section>
-        <img src="https://i.pinimg.com/originals/16/e5/7e/16e57e6693771343048239e2d5e97dfc.gif" alt="">
+        <!-- <img src="../../assets/rain.webm" alt=""> -->
+        <video autoplay loop muted>
+            <source src="../../assets/rain.webm" type="video/webm" />
+            Your browser does not support the video tag.
+        </video>
     </section>
 </template>
 
 <style scoped>
-    section {
-        width: 100%;
-        height: 90vh;
-        background-color: #ffffff;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        z-index: -1;
-        overflow-y: hidden;
-    }
-     
-    img {
-        width: 100%;
-        height: 1005;
-        filter: brightness(0.8) blur(5px);
-    }
+section {
+    width: 100%;
+    height: 90vh;
+    background-color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: -1;
+    overflow-y: hidden;
+}
 
-    h1 {
-        color: #999;
-    }
+img, video {
+    width: 100%;
+    /* height: 100%; */
+    filter: brightness(0.8) blur(5px);
+}
+
+h1 {
+    color: #999;
+}
 </style>
