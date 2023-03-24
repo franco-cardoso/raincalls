a836c6b05a57d25633531e049d905b113113dac1
franco-cardoso <francolkk788@gmail.com>
2023-03-24 02:06:45 -0300

    rating

---
+++ src/components/reviews/Review.vue
@@ -0,0 +1,36 @@
+<script setup>
+import StarRating from "vue-star-rating";
+</script>
+
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <div class="stars">
+            <star-rating
+                :rating="rating"
+                :read-only="true"
+                :increment="0.01"
+                :show-rating="false"
+                :rounded-corners="true"
+                :star-size="30"
+                border-color="#000"
+            ></star-rating>
+            <span>{{ rating }}</span>
+        </div>
+        <span>{{ amount }} {{ title }} reviews</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Review",
+    components: {
+        StarRating,
+    },
+    props: {
+        title: String,
+        rating: Number,
+        amount: Number,
+    },
+};
+</script>
--- src/components/reviews/Reviews.vue
+++ src/components/reviews/Reviews.vue
@@ -1,20 +1,45 @@
+<script setup>
+import Review from "./Review.vue";
+</script>
+
 <template>
     <section>
-        <h1>REVIEWS</h1>
+        <h1>RATINGS</h1>
+        <div class="reviews-container">
+            <Review title="Google" :rating="getRandomRating()" :amount="54"></Review>
+            <Review title="Glass Door" :rating="getRandomRating()" :amount="54"></Review>
+            <Review title="Indeed" :rating="getRandomRating()" :amount="54"></Review>
+            <Review title="Facebook" :rating="getRandomRating()" :amount="54"></Review>
+        </div>
     </section>
 </template>
 
 <style scoped>
-    section {
-        height: 500px;
-        width: 100%;
-        background-color: #505050;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+section {
+    height: 500px;
+    width: 100%;
+    background-color: #505050;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: 7rem;
+}
 
-    h1 {
-        color: var(--dark-text);
-    }
+.reviews-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 6rem;
+}
+
+h1 {
+    color: var(--dark-text);
+    font-size: 50px;
+}
 </style>
+
+<script>
+const getRandomRating = () => (Math.random() * 5).toFixed(1)
+
+</script>
