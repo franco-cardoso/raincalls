f33b5cf6ec624656e44b300747025e2f3e864d55
franco-cardoso <francolkk788@gmail.com>
2023-03-25 20:02:11 -0300

    css fix

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -31,7 +31,7 @@ import { useRoute } from "vue-router";
     top: v-bind("scrollPos <= headerSizeChange ? '-20vh' : '0px' ");
     height: v-bind("scrollPos <= headerSizeChange ? '100vh' : '80px' ");
     /* top: v-bind("scrollPos <= headerSizeChange ? '0' : '-80px' "); */
-    padding: v-bind("scrollPos <= headerSizeChange ? '0 2rem 0 0' : '0px 3rem' ");
+    padding: v-bind("scrollPos <= headerSizeChange ? '0 2rem' : '0px 3rem' ");
     background-color: v-bind("scrollPos <= headerSizeChange ? '' : 'white' ");
     box-shadow: v-bind("scrollPos <= headerSizeChange ? '' : '0px -35px 50px 10px black'");
 }
@@ -105,7 +105,6 @@ nav {
         height: 230px;
         padding: 0 10px;
         width: 100%;
-        margin: 0;
     }
 
     .lw-dyn {
@@ -118,9 +117,6 @@ nav {
         width: 100%;
     }
 
-    .header-container div:nth-child(1) {
-        margin-left: 0px;
-    }
 
     nav {
         align-self: flex-start;
